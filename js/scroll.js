6d94893538390c373365cd4de481f995642c93a0
Wathanan Lucas <wathanansantos@gmail.com>
2021-09-08 13:35:11 -0300

    acrescentei o about me

---
+++ js/scroll.js
@@ -0,0 +1,22 @@
+const menuItems = document.querySelectorAll('.menu a[href^="#"]')
+const nav = document.querySelector('.nav');
+
+menuItems.forEach(item => {
+    item.addEventListener('click', scrollToId);
+})
+
+function scrollToId(event) {
+
+    nav.classList.remove('active')
+
+    event.preventDefault();
+
+    const element = event.target;
+    const id = element.getAttribute('href');
+    const section = document.querySelector(id).offsetTop;
+    
+    window.scroll({
+        top: section - 35,
+    });
+}
+
