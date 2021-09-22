f3b25cfbb796d35579399a3cd9ea55042db87966
Wathanan Lucas <wathanansantos@gmail.com>
2021-09-22 15:56:03 -0300

    button top

--- js/scroll.js
+++ js/scroll.js
@@ -1,9 +1,21 @@
 const menuItems = document.querySelectorAll('.menu a[href^="#"]')
 const nav = document.querySelector('.nav');
+const topo = document.querySelector('.icon a[href^="#"]')
 
 menuItems.forEach(item => {
     item.addEventListener('click', scrollToId);
 })
+
+topo.addEventListener('click', scrollTopo)
+
+function scrollTopo(event) {
+    event.preventDefault();
+
+    window.scroll({
+        top: 0
+    })
+
+}
 
 function scrollToId(event) {
 
@@ -14,9 +26,16 @@ function scrollToId(event) {
     const element = event.target;
     const id = element.getAttribute('href');
     const section = document.querySelector(id).offsetTop;
-    
+
+
     window.scroll({
         top: section - 15,
     });
+    
+   
 }
+
+
+
+
 
